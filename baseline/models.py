bab00ff4ca7ef8c383b497710ea3736aadac0c5b
SSSKJ <skjguan@vt.edu>
2025-01-29 08:31:17 -0800

    add EnergyModel and Mahalanobis, fix bugs in GKDE KL_prior

--- baseline/models.py
+++ baseline/models.py
@@ -467,7 +467,7 @@ class SGCN(nn.Module):
         self.params["seed"] = args.gkde_seed
         self.params["dim_hidden"] = args.gkde_dim_hidden
         self.params["dropout_prob"] = args.gkde_dropout_prob
-        self.params["use_kernel"] = bool(args.gkde_use_kernel)
+        self.params["use_kernel"] = bool(args.gkde_use_kernel) # False
         self.params["lambda_1"] = args.gkde_lambda_1
         self.params["teacher_training"] = bool(args.gkde_teacher_training)
         self.params["use_bayesian_dropout"] = bool(args.gkde_use_bayesian_dropout)
@@ -634,6 +634,8 @@ class SGCN(nn.Module):
             KL_prior = D.kl.kl_divergence(dirichlet, dirichlet_prior)
             KL_prior = loss_reduce(KL_prior, reduction='sum')
             losses['KL_prior'] = self.params["lambda_1"] * KL_prior / n_nodes
+        else:
+            losses['KL_prior'] = 0
 
         # KL divergence for teacher training
         if self.params["teacher_training"]:
@@ -649,11 +651,23 @@ class SGCN(nn.Module):
 
             y_teacher = self.y_teacher.to(prediction.soft.device).detach()
             lambda_2 = min(1.0, self.epoch * 1.0 / 200)
-            categorical_pred = D.Categorical(prediction.soft)
+            try:
+                categorical_pred = D.Categorical(prediction.soft)
+            except:
+                print(sum(prediction.soft <= 0))
+                print(torch.isnan(prediction.soft).sum().item())
+                mask = (prediction.soft <= 0) | torch.isnan(prediction.soft)
+                print(prediction.soft[mask.any(dim=1)])
+                print(prediction.alpha[mask.any(dim=1)])
+                exit(1)
             categorical_teacher = D.Categorical(y_teacher)
             KL_teacher = D.kl.kl_divergence(categorical_pred, categorical_teacher)
             KL_teacher = loss_reduce(KL_teacher, reduction='sum')
             losses['KL_teacher'] = lambda_2 * KL_teacher / n_nodes
+
+        # print(losses['BR'])
+        # print(losses['KL_prior'])
+        # print(losses['KL_teacher'])
 
         return losses['BR'] + losses['KL_prior'] + losses['KL_teacher']
 
@@ -686,6 +700,8 @@ class SGCN(nn.Module):
             KL_prior = D.kl.kl_divergence(dirichlet, dirichlet_prior)
             KL_prior = loss_reduce(KL_prior, reduction='sum')
             losses['KL_prior'] = self.params["lambda_1"] * KL_prior / n_nodes
+        else:
+            losses['KL_prior'] = 0
 
         # KL divergence for teacher training
         if self.params["teacher_training"]:
@@ -701,7 +717,15 @@ class SGCN(nn.Module):
 
             y_teacher = self.y_teacher.to(prediction.soft.device)
             lambda_2 = min(1.0, self.epoch * 1.0 / 200)
-            categorical_pred = D.Categorical(prediction.soft)
+            try:
+                categorical_pred = D.Categorical(prediction.soft)
+            except:
+                print(sum(prediction.soft <= 0))
+                print(torch.isnan(prediction.soft).sum().item())
+                mask = (prediction.soft <= 0) | torch.isnan(prediction.soft)
+                print(prediction.soft[mask.any(dim=1)])
+                print(prediction.alpha[mask.any(dim=1)])
+                exit(1)
             categorical_teacher = D.Categorical(y_teacher)
             KL_teacher = D.kl.kl_divergence(categorical_pred, categorical_teacher)
             KL_teacher = loss_reduce(KL_teacher, reduction='sum')
@@ -974,4 +998,275 @@ class GNNSafe(nn.Module):
         else:
             loss = sup_loss
 
+        return loss
+    
+
+class EnergyModel(nn.Module):
+    def __init__(self, d, c, args):
+        super(EnergyModel, self).__init__()
+        if args.backbone == 'gcn':
+            self.encoder = GCN(in_channels=d,
+                        hidden_channels=args.hidden_channels,
+                        out_channels=c,
+                        num_layers=args.num_layers,
+                        dropout=args.dropout,
+                        use_bn=args.use_bn)
+        elif args.backbone == 'mlp':
+            self.encoder = MLP(in_channels=d, hidden_channels=args.hidden_channels,
+                        out_channels=c, num_layers=args.num_layers,
+                        dropout=args.dropout)
+        elif args.backbone == 'sgc':
+            self.encoder = SGC(in_channels=d, out_channels=c, hops=args.hops)
+        elif args.backbone == 'gat':
+            self.encoder = GAT(d, args.hidden_channels, c, num_layers=args.num_layers,
+                        dropout=args.dropout, use_bn=args.use_bn, heads=args.gat_heads, out_heads=args.out_heads)
+        else:
+            raise NotImplementedError
+
+    def reset_parameters(self):
+        self.encoder.reset_parameters()
+
+    def forward(self, dataset, device):
+        x, edge_index = dataset.x.to(device), dataset.edge_index.to(device)
+        return self.encoder(x, edge_index)
+
+    def detect(self, dataset, node_idx, device, args):
+
+        logits = self.encoder(dataset.x.to(device), dataset.edge_index.to(device))[node_idx]
+        if args.dataset in ('proteins', 'ppi'):
+            logits = torch.stack([logits, torch.zeros_like(logits)], dim=2)
+            neg_energy = args.T * torch.logsumexp(logits / args.T, dim=-1).sum(dim=1)
+        else:
+            neg_energy = args.T * torch.logsumexp(logits / args.T, dim=-1)
+        return neg_energy
+
+    def loss_compute(self, dataset_ind, dataset_ood, criterion, device, args):
+
+        train_in_idx, _ = dataset_ind.splits['train'], dataset_ood.node_idx
+
+        logits_in = self.encoder(dataset_ind.x.to(device), dataset_ind.edge_index.to(device))[train_in_idx]
+
+        if args.dataset in ('proteins', 'ppi'):
+            sup_loss = criterion(logits_in, dataset_ind.y[train_in_idx].to(device).to(torch.float))
+        else:
+            pred_in = F.log_softmax(logits_in, dim=1)
+            sup_loss = criterion(pred_in, dataset_ind.y[train_in_idx].squeeze(1).to(device))
+
+        loss = sup_loss
+
+        return loss
+    
+
+# noinspection PyUnreachableCode
+class Mahalanobis(nn.Module):
+    def __init__(self, d, c, args):
+        super(Mahalanobis, self).__init__()
+        if args.backbone == 'gcn':
+            self.encoder = GCN(in_channels=d,
+                        hidden_channels=args.hidden_channels,
+                        out_channels=c,
+                        num_layers=args.num_layers,
+                        dropout=args.dropout,
+                        use_bn=args.use_bn)
+        elif args.backbone == 'mlp':
+            self.encoder = MLP(in_channels=d, hidden_channels=args.hidden_channels,
+                        out_channels=c, num_layers=args.num_layers,
+                        dropout=args.dropout)
+        elif args.backbone == 'appnp':
+            self.encoder = APPNP_Net(d, args.hidden_channels, c, dropout=args.dropout)
+        elif args.backbone == 'gat':
+            self.encoder = GAT(d, args.hidden_channels, c, num_layers=args.num_layers,
+                        dropout=args.dropout, use_bn=args.use_bn, heads=args.gat_heads, out_heads=args.out_heads)
+        else:
+            raise NotImplementedError
+
+    def reset_parameters(self):
+        self.encoder.reset_parameters()
+
+    def forward(self, dataset, device):
+        x, edge_index = dataset.x.to(device), dataset.edge_index.to(device)
+        return self.encoder(x, edge_index)
+
+    def detect(self, train_set, train_idx, test_set, node_idx, device, args):
+        temp_list = self.encoder.feature_list(train_set.x.to(device), train_set.edge_index.to(device))[1]
+        num_output = len(temp_list)
+        feature_list = np.empty(num_output)
+        count = 0
+        for out in temp_list:
+            feature_list[count] = out.size(1)
+            count += 1
+        # print('get sample mean and covariance', count)
+        num_classes = len(torch.unique(train_set.y))
+        sample_mean, precision = self.sample_estimator(num_classes, feature_list, train_set, train_idx, device)
+        in_score = self.get_Mahalanobis_score(test_set, node_idx, device, num_classes, sample_mean, precision, count-1, args.noise)
+        return torch.Tensor(in_score)
+
+    def get_Mahalanobis_score(self, test_set, node_idx, device,  num_classes, sample_mean, precision, layer_index, magnitude):
+        '''
+        Compute the proposed Mahalanobis confidence score on input dataset
+        return: Mahalanobis score from layer_index
+        '''
+        self.encoder.eval()
+        Mahalanobis = []
+
+        data, target = test_set.x.to(device), test_set.y[node_idx].to(device)
+        edge_index = test_set.edge_index.to(device)
+        data, target = Variable(data, requires_grad=True), Variable(target)
+
+        out_features = self.encoder.intermediate_forward(data, edge_index, layer_index)[node_idx]
+        out_features = out_features.view(out_features.size(0), out_features.size(1), -1)
+        out_features = torch.mean(out_features, 2)
+
+        # compute Mahalanobis score
+        gaussian_score = 0
+        for i in range(num_classes):
+            batch_sample_mean = sample_mean[layer_index][i]
+            zero_f = out_features.data - batch_sample_mean
+            term_gau = -0.5 * torch.mm(torch.mm(zero_f, precision[layer_index]), zero_f.t()).diag()
+            if i == 0:
+                gaussian_score = term_gau.view(-1, 1)
+            else:
+                gaussian_score = torch.cat((gaussian_score, term_gau.view(-1, 1)), 1)
+
+        # Input_processing
+        sample_pred = gaussian_score.max(1)[1]
+        batch_sample_mean = sample_mean[layer_index].index_select(0, sample_pred)
+        zero_f = out_features - Variable(batch_sample_mean)
+        pure_gau = -0.5 * torch.mm(torch.mm(zero_f, Variable(precision[layer_index])), zero_f.t()).diag()
+        loss = torch.mean(-pure_gau)
+        datagrad = autograd.grad(loss,data)[0]
+
+        gradient = torch.ge(datagrad.data, 0)
+        gradient = (gradient.float() - 0.5) * 2
+        '''gradient.index_copy_(1, torch.LongTensor([0]).to(device),
+                     gradient.index_select(1, torch.LongTensor([0]).to(device)) / (63.0 / 255.0))
+        gradient.index_copy_(1, torch.LongTensor([1]).to(device),
+                     gradient.index_select(1, torch.LongTensor([1]).to(device)) / (62.1 / 255.0))
+        gradient.index_copy_(1, torch.LongTensor([2]).to(device),
+                     gradient.index_select(1, torch.LongTensor([2]).to(device)) / (66.7 / 255.0))'''
+
+        tempInputs = torch.add(data.data, -magnitude, gradient)
+        with torch.no_grad():
+            noise_out_features = self.encoder.intermediate_forward(tempInputs, edge_index, layer_index)[node_idx]
+        noise_out_features = noise_out_features.view(noise_out_features.size(0), noise_out_features.size(1), -1)
+        noise_out_features = torch.mean(noise_out_features, 2)
+        noise_gaussian_score = 0
+        for i in range(num_classes):
+            batch_sample_mean = sample_mean[layer_index][i]
+            zero_f = noise_out_features.data - batch_sample_mean
+            term_gau = -0.5 * torch.mm(torch.mm(zero_f, precision[layer_index]), zero_f.t()).diag()
+            if i == 0:
+                noise_gaussian_score = term_gau.view(-1, 1)
+            else:
+                noise_gaussian_score = torch.cat((noise_gaussian_score, term_gau.view(-1, 1)), 1)
+
+        noise_gaussian_score, _ = torch.max(noise_gaussian_score, dim=1)
+        Mahalanobis.extend(-noise_gaussian_score.cpu().numpy())
+
+        return np.asarray(Mahalanobis, dtype=np.float32)
+
+    def sample_estimator(self, num_classes, feature_list, dataset, node_idx, device):
+        """
+        compute sample mean and precision (inverse of covariance)
+        return: sample_class_mean: list of class mean
+                 precision: list of precisions
+        """
+        import sklearn.covariance
+
+        self.encoder.eval()
+        group_lasso = sklearn.covariance.EmpiricalCovariance(assume_centered=False)
+        correct = 0
+        num_output = len(feature_list)
+        num_sample_per_class = np.empty(num_classes)
+        num_sample_per_class.fill(0)
+        list_features = []
+        for i in range(num_output):
+            temp_list = []
+            for j in range(num_classes):
+                temp_list.append(0)
+            list_features.append(temp_list)
+
+        total = len(node_idx)
+        output, out_features = self.encoder.feature_list(dataset.x.to(device), dataset.edge_index.to(device))
+        output = output[node_idx]
+
+        # get hidden features
+        for i in range(num_output):
+            out_features[i] = out_features[i].view(out_features[i].size(0), out_features[i].size(1), -1)
+            out_features[i] = torch.mean(out_features[i].data, 2)
+
+        # compute the accuracy
+        pred = output.data.max(1)[1]
+        target = dataset.y[node_idx].to(device)
+        equal_flag = pred.eq(target).cpu()
+        correct += equal_flag.sum()
+
+        # construct the sample matrix
+        for i in range(total):
+            label = target[i]
+            if num_sample_per_class[label] == 0:
+                out_count = 0
+                for out in out_features:
+                    list_features[out_count][label] = out[i].view(1, -1)
+                    out_count += 1
+            else:
+                out_count = 0
+                for out in out_features:
+                    list_features[out_count][label] \
+                        = torch.cat((list_features[out_count][label], out[i].view(1, -1)), 0)
+                    out_count += 1
+            num_sample_per_class[label] += 1
+
+        sample_class_mean = []
+        out_count = 0
+        for num_feature in feature_list:
+            temp_list = torch.Tensor(num_classes, int(num_feature)).to(device)
+            for j in range(num_classes):
+                if isinstance(list_features[out_count][j], int):
+                    temp_list[j] = torch.tensor(list_features[out_count][j])
+                else:
+                    temp_list[j] = torch.mean(list_features[out_count][j], 0)
+            sample_class_mean.append(temp_list)
+            out_count += 1
+
+        precision = []
+        for k in range(num_output):
+            X = 0
+            for i in range(num_classes):
+                if i == 0:
+                    if len((list_features[k][i] - sample_class_mean[k][i]).shape) == 1:
+                        X = (list_features[k][i] - sample_class_mean[k][i]).unsqueeze(0)
+                    else:
+                        X = (list_features[k][i] - sample_class_mean[k][i])
+
+                else:
+                    try:
+                        if len((list_features[k][i] - sample_class_mean[k][i]).shape) == 1:
+                            X = torch.cat((X, (list_features[k][i] - sample_class_mean[k][i]).unsqueeze(0)), 0)
+                        else:
+                            X = torch.cat((X, list_features[k][i] - sample_class_mean[k][i]), 0)
+                    except:
+                        print(X.shape)
+                        print((list_features[k][i] - sample_class_mean[k][i]).shape)
+                        exit()
+
+            # find inverse
+            group_lasso.fit(X.cpu().numpy())
+            temp_precision = group_lasso.precision_
+            temp_precision = torch.from_numpy(temp_precision).float().to(device)
+            precision.append(temp_precision)
+
+        # print('\n Training Accuracy:({:.2f}%)\n'.format(100. * correct / total))
+
+        return sample_class_mean, precision
+
+    def loss_compute(self, dataset_ind, dataset_ood, criterion, device, args):
+
+        train_idx = dataset_ind.splits['train']
+        logits_in = self.encoder(dataset_ind.x.to(device), dataset_ind.edge_index.to(device))[train_idx]
+        if args.dataset in ('proteins', 'ppi'):
+            loss = criterion(logits_in, dataset_ind.y[train_idx].to(device).to(torch.float))
+        else:
+            pred_in = F.log_softmax(logits_in, dim=1)
+            loss = criterion(pred_in, dataset_ind.y[train_idx].squeeze(1).to(device))
         return loss
